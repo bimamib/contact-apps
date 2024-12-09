8159e3e8cb0214d5848d0c39c5ad7290639b0cc0
ʙ ɪ ᴍ ᴀ ᴀ ᴀ <bimaprast1601@gmail.com>
2024-12-10 01:48:50 +0700

    Fix Navigation Router

--- app/edit/[id]/page.tsx
+++ app/edit/[id]/page.tsx
@@ -2,7 +2,7 @@
 "use client";
 
 import { useState, useEffect } from "react";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 import { ContactForm } from "@/components/ContactForm";
 import { Contact } from "@/lib/types";
 
